d9a0b65689a4d04f30726dcf99a5c7bef01c7ef6
Lee SeoJune <sung07288346@gmail.com>
2022-03-04 11:51:39 +0900

    📑 :: CountRepository

---
+++ src/count/domain/repository/count.repository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Count } from '../count.entity';
+
+@EntityRepository(Count)
+export class CountRepository extends Repository<Count> {}
