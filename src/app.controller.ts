540f4654cae9c8e11adf7d344ff910b72d0e3263
Lee SeoJune <sung07288346@gmail.com>
2022-03-04 22:10:29 +0900

    ⚡️ :: queryCountry api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { RealIP } from 'nestjs-real-ip';
 import { ClickRequset } from './dto/request/click.request';
@@ -8,7 +8,17 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('click')
-  async get(@RealIP() ip: string, @Query() clickRequest: ClickRequset) {
+  async click(@RealIP() ip: string, @Query() clickRequest: ClickRequset) {
     return this.appService.click(ip, clickRequest);
+  }
+
+  @Get('countries')
+  async queryCountryList(@Query('page') page: number) {
+    return this.appService.queryCountryList(page);
+  }
+
+  @Get('countries/:country_code')
+  async query(@Param('country_code') countryCode: string) {
+    return this.appService.queryCountry(countryCode);
   }
 }
