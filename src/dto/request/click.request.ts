310ead31e4e39267bcdeff8e9e64b1d1d5e16aff
Lee SeoJune <sung07288346@gmail.com>
2022-03-06 22:48:10 +0900

    ⚡️ :: max to 1000

--- src/dto/request/click.request.ts
+++ src/dto/request/click.request.ts
@@ -4,7 +4,7 @@ import { IsNumber, Max, Min } from 'class-validator';
 export class ClickRequset {
   @IsNumber()
   @Min(0)
-  @Max(800)
+  @Max(1000)
   @Type(() => Number)
   readonly click = 0;
   readonly authorization: string;
